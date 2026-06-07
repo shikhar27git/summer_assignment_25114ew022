fda6426628303989d05c55a5edc40835e050ce98
shikhar27git <shikharp84@gmail.com>
2026-06-07 22:52:41 +0530

    day 7

---
+++ day 7/q25.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int n,fact=1;
+    printf("Enter thr no=");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++)
+    {
+        fact=fact*i;
+    }
+    printf("The ans=%d",fact);
+    return 0;
+}
